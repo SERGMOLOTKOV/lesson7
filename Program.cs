105407024a6ad63efa9762231d275ed4cfa78a5d
SERGMOLOTKOV <156708384+SERGMOLOTKOV@users.noreply.github.com>
2024-02-22 19:27:59 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int[] array = { 1, 2, 3, 4, 5 }; // Произвольный массив
+
+        // Выводим элементы массива, начиная с конца
+        PrintArrayReversed(array, array.Length - 1);
+    }
+
+    static void PrintArrayReversed(int[] array, int index)
+    {
+        // Базовый случай: если индекс выходит за границы массива, завершаем рекурсию
+        if (index < 0)
+            return;
+
+        // Выводим текущий элемент массива
+        Console.WriteLine(array[index]);
+
+        // Рекурсивно вызываем функцию для предыдущего элемента
+        PrintArrayReversed(array, index - 1);
+    }
+}
